5926509fa2db7fd27ba4886c4fd97fdc8d5f4b87
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-03-18 00:46:57 +0000

    created math

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+function doMathClicked() {
+  var w = 5
+  var l = 3
+  document.getElementById("button-area").innerHTML += w*l + " cm<sup>2</sup>"
+  document.getElementById("button-perimeter").innerHTML += 2 * (w+l) + " cm"
+}
